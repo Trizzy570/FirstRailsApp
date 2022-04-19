5ebdf5dc2fb4122f3559aad596dc3e41fdbb9da4
alex <troy.alexj570@gmail.com>
2022-04-18 21:09:28 -0400

    dot dot dot

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    Rails.application.routes.draw do
+  
       root "articles#index"
+
+    #  post "/articles#new"
     
-      get "/articles", to: "articles#index"
-    end
-    
-    get "/articles", to: "articles#index"
+     resources :articles
   
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end
-  
+   
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
